4a4ed629954859338ba7a787ff1fb642a5ade4a4
PanhaCs <sopanhachanda@gmail.com>
2024-10-24 01:01:26 +0700

    impliment user pages by panha

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,36 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/user1', [UserController::class, 'index']);
-Route::get('/', function () {
-    // return view('admin/index');
-    return view('admin/login');
-});
+Route::get('/users', [UserController::class, 'index']);
+// Route::get('/', function () {
+//     // return view('admin/index');
+//     return view('admin/login');
+// });
 
 
 Route::get('/dashboard', function () {
 
     return view('admin/main');
+});
+
+
+
+Route::get('/', function () {
+    return view('user/index');
+});
+
+Route::get('/product', function () {
+    return view('user/product');
+});
+
+Route::get('/blog', function () {
+    return view('user/blog');
+});
+
+Route::get('/about', function () {
+    return view('user/about');
+});
+
+Route::get('/contact', function () {
+    return view('user/contact');
 });
